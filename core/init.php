1ed59d5fe93b95ab75ef0cdfbc90952daaecec34
EWsetiawan <ekowahyudisetiawan@gmail.com>
2019-06-22 03:14:43 +0700

    Add exiting file

---
+++ core/init.php
@@ -0,0 +1,13 @@
+<?php 
+
+$db = mysqli_connect('localhost','root','','tutorial');
+
+if (mysqli_connect_errno()){
+	echo 'database connection failed with following error: ' . mysqli_connect_errno();
+	die();
+}
+
+
+
+
+ ?>
