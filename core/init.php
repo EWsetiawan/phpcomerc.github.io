172d77718f363576086c3e5ce9982abf698df86a
EWsetiawan <ekowahyudisetiawan@gmail.com>
2019-06-22 11:23:02 +0700

    Update navigation bar

--- core/init.php
+++ core/init.php
@@ -5,6 +5,7 @@ $db = mysqli_connect('localhost','root','','tutorial');
 if (mysqli_connect_errno()){
 	echo 'database connection failed with following error: ' . mysqli_connect_errno();
 	die();
+	
 }
 
 
